d7575898a201cf371ff95956e014923ed6a8ac82
LucasPires192 <lucas.andrade192@etec.sp.gov.br>
2025-04-04 20:14:59 -0300

    Finalizacao da primeira versao do programa

--- listaCarros/Program.cs
+++ listaCarros/Program.cs
@@ -5,6 +5,7 @@ class Carros
     {
         //Criação de uma lista para armazenar as categoria de carros
         string[] categoria = {"Hatch", "Sedan", "SUV"};
+        //Criação de uma lista com os modelos de cada categoria        
         string[,] hatch = {
             {"Fit", "50.000", "1.5", "Flex"},
             {"Corsa", "35.000", "1.6", "Flex"},
@@ -20,13 +21,13 @@ class Carros
             {"ix35", "68.000", "2.0", "Flex"},
             {"SantaFe", "85.000", "2.5", "Gasolina"}
         };
-        bool loop = true;
+        
         string resposta;
 
-        Console.WriteLine("Escolha uma categoria de carros: ");
-
-        while(loop)
+        while(true)
         {
+            Console.WriteLine("Escolha uma categoria de carros: ");
+            
             //Mostra os valores da lista categoria para o usuario escolher
             for (int i = 0; i < categoria.Length; i++)
             {
@@ -38,69 +39,60 @@ class Carros
             switch (resposta)
             {
                 case "1":
+                    Console.Clear();
+                    Console.WriteLine("Hatch: ");
                     for (int i = 0; i < hatch.GetLength(0); i++)
                     {
                         Console.WriteLine(i + 1 + ". " + hatch[i, 0]);
                     }
-                    loop = false;
                     break;
                 case "2":
+                    Console.Clear();
+                    Console.WriteLine("Sedan: ");
                     for (int i = 0; i < sedan.GetLength(0); i++)
                     {
                         Console.WriteLine(i + 1 + ". " + sedan[i, 0]);
                     }
-                    loop = false;
                     break;
                 case "3":
+                    Console.Clear();
+                    Console.WriteLine("SUV: ");
                     for (int i = 0; i < suv.GetLength(0); i++)
                     {
                         Console.WriteLine(i + 1 + ". " + suv[i, 0]);
                     }
-                    loop = false;
                     break;
                 default:
+                    Console.Clear();
                     Console.WriteLine("Valor Inválido! Digite novamente!");
-                    break;
+                    continue;
             }
-        }
-
-        while(loop)
-        {
-            //Mostra os valores da lista categoria para o usuario escolher
-            for (int i = 0; i < categoria.Length; i++)
+            while(true)
             {
-                Console.WriteLine(i + 1 + ". " + categoria[i]);
-            }
+                resposta = Console.ReadLine();
 
-            resposta = Console.ReadLine();
-
-            switch (resposta)
-            {
-                case "1":
-                    for (int i = 0; i < hatch.GetLength(0); i++)
-                    {
-                        Console.WriteLine(i + 1 + ". " + hatch[i, 0]);
-                    }
-                    loop = false;
-                    break;
-                case "2":
-                    for (int i = 0; i < sedan.GetLength(0); i++)
-                    {
-                        Console.WriteLine(i + 1 + ". " + sedan[i, 0]);
-                    }
-                    loop = false;
-                    break;
-                case "3":
-                    for (int i = 0; i < suv.GetLength(0); i++)
-                    {
-                        Console.WriteLine(i + 1 + ". " + suv[i, 0]);
-                    }
-                    loop = false;
-                    break;
-                default:
-                    Console.WriteLine("Valor Inválido! Digite novamente!");
-                    break;
+                switch (resposta)
+                {
+                    case "1":
+                        Console.Clear();
+                        Console.WriteLine($"Modelo: {hatch[0 , 0]}\n Km: {hatch[0, 1]}\n Motor: {hatch[0, 2]}\n Combustivel: {hatch[0, 3]}");
+                        break;
+                    case "2":
+                        Console.Clear();
+                        Console.WriteLine($"Modelo: {sedan[0 , 0]}\n Km: {sedan[0, 1]}\n Motor: {sedan[0, 2]}\n Combustivel: {sedan[0, 3]}");
+                        break;
+                    case "3":
+                        Console.Clear();
+                        Console.WriteLine($"Modelo: {suv[0 , 0]}\n Km: {suv[0, 1]}\n Motor: {suv[0, 2]}\n Combustivel: {suv[0, 3]}");
+                        break;
+                    default:
+                        Console.Clear();
+                        Console.WriteLine("Valor Inválido! Digite novamente!");
+                        continue;
+                }
+                break;
             }
+            break;
         }
     }
 }
